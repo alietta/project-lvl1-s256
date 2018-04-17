b00dda9b111b02e788e1fe9a20c39743248c6801
Tatiana Kudashova <kudashova93@gmail.com>
2018-04-17 20:09:02 +0400

    get player name

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
+import {getPlayerName} from '..'
 var colors = require('colors');
+
 console.log('Welcome,'.magenta + ' to the ' + 'Brain'.bold.black + ' Games!');
+console.log('Hello, ' + `${getPlayerName()}`.bold.black + '!');
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import readlineSync from 'readline-sync';
+export const getPlayerName = () => {
+  const playerName = readlineSync.question('May I have your name? ');
+  return playerName
+}
