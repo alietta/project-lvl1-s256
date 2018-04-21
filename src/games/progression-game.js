a391e3109237408f84f38c2e516ea1359bb29eb7
Tatiana Kudashova <kudashova93@gmail.com>
2018-04-21 19:38:26 +0400

    progression game

---
+++ src/games/progression-game.js
@@ -0,0 +1,22 @@
+import { cons } from 'hexlet-pairs';
+import { _ } from 'lodash';
+import playGame from '../component/brain_game';
+import randomInt from '../component/prepare_data';
+
+const task = 'What number is missing in this progression?';
+const progLen = 10;
+const makeProgression = () => {
+  const step = randomInt(1, 5);
+  return _.range(progLen).map((elem, i) => elem + 1 + (step * i));
+};
+const gameRule = () => {
+  const arr = makeProgression();
+  const index = randomInt(0, progLen - 1);
+  const rightAnswer = `${arr[index]}`;
+  arr[index] = '..';
+  const question = `${arr.join(' ')}`;
+  return cons(question, rightAnswer);
+};
+export default () => {
+  playGame(task, gameRule);
+};
